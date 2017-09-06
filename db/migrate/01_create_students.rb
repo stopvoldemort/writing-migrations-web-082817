d2ad6822461fc9cdd5d896d4a40b70823b608015
stopvoldemort <david.squires1@gmail.com>
2017-09-06 17:35:51 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,14 @@
-class CreateStudents < ActiveRecord::Migration
+class CreateStudents < ActiveRecord::Migration[4.2]
+
+  drop_table 'students' if ActiveRecord::Base.connection.table_exists? 'students'
+  create_table :students
+
+
+  def change
+    drop_table 'students' if ActiveRecord::Base.connection.table_exists? 'students'
+    create_table :students do |t|
+      t.string :name
+    end
+  end
+
 end
